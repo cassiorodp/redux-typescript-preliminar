aab80b7fa82afff5a0d4bc1e47543b3299e923f6
Cássio Pereira <cassiorodp@gmail.com>
2023-02-06 17:45:41 -0300

    feat: display results

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -25,6 +25,15 @@ const RepositoriesList: React.FC = () => {
         />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && (
+        <ul>
+          {data.map((name) => (
+            <li key={name}>{name}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
